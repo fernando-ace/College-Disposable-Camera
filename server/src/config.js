9ac1f1f520e49bae7e5985e0938cd9be38e04c37
Fernando Acevedo <FernandoJosueAcevedo@gmail.com>
2026-05-20 02:01:02 -0500

    Fix production guest route deployment

--- server/src/config.js
+++ server/src/config.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 
 const maxFileSizeMb = Number(process.env.MAX_FILE_SIZE_MB || 10);
 
+function trimTrailingSlash(value) {
+  return value.replace(/\/+$/, "");
+}
+
 module.exports = {
   port: Number(process.env.PORT || 4000),
   jwtSecret: process.env.JWT_SECRET || "dev-change-me",
-  clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
-  serverUrl: process.env.SERVER_URL || "http://localhost:4000",
+  clientUrl: trimTrailingSlash(process.env.CLIENT_URL || "http://localhost:5173"),
+  serverUrl: trimTrailingSlash(process.env.SERVER_URL || "http://localhost:4000"),
   maxFileSizeMb,
   maxFileSizeBytes: maxFileSizeMb * 1024 * 1024,
   supabaseUrl: process.env.SUPABASE_URL,
